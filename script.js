a4a7e370e272fc8cf6f5bf26348a0f12265573ec
Tauqeer Alam <mdta2108@gmail.com>
2023-06-23 21:52:50 +0530

    adding files

--- script.js
+++ script.js
@@ -1,26 +1,129 @@
-const loginForm = document.getElementById('login');
-      loginForm.addEventListener('submit', handleLogin);
 
-      function handleLogin(event) {
-        event.preventDefault();
-        const username = document.getElementById('username').value;
-        const password = document.getElementById('password').value;
-        const url = 'http://localhost:3000/users'; // Replace with your JSON server endpoint
+async function login() {
+  event.preventDefault();
 
-        fetch(url)
-          .then(response => response.json())
-          .then(users => {
-            const user = users.find(u => u.username === username && u.password === password);
-            if (user) {
-              console.log('Login successful');
-              // Perform any necessary actions for a successful login
-            } else {
-              console.log('Login failed');
-              // Perform any necessary actions for a failed login
-            }
-          })
-          .catch(error => {
-            console.log('An error occurred:', error);
-            // Perform any necessary error handling
-          });
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  try {
+      const response = await fetch(`http://localhost:2222/users`)
+      const data = await response.json();
+      console.log(data);
+      const matcheduser = data.find(user => user.username === username && user.password === password);
+      if (matcheduser) {
+          sessionStorage.setItem("user", JSON.stringify(matcheduser));
+
+          sessionStorage.setItem("islogin", "true");
+
+          window.location.href = "index.html";
       }
+
+      else {
+          alert('invalid login');
+      }
+  }
+  catch (error) {
+      console.error('error', error);
+
+      alert("please try again");
+  }
+
+}
+
+//==========================================================================================================
+
+function addBlogs(){
+  document.getElementById('myForm').addEventListener('submit',function(event){
+    alert("Blog added");
+      event.preventDefault();
+      const title=document.getElementById('title').value;
+      const content=document.getElementById('content').value;
+      const author=document.getElementById('author').value;
+
+      const blog={
+          title:title,
+          content:content,
+          author:author
+      };
+
+
+      fetch("http://localhost:2222/blogs",{
+          method:'post',
+          headers:{
+              'content-Type':'application/json'
+          },
+          body:JSON.stringify(blog)
+      })
+      .then(response => response.json())
+      .then(responseblog =>{
+          console.log('Registered successfully');
+          window.location.href='index.html'
+      });
+  });
+}
+//===================================================================================
+fetch('http://localhost:2222/blogs')
+  .then(response=> {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    appendData(data);
+  })
+  .catch(err=> {
+    console.log(err);
+  });
+    
+
+  function appendData(data){
+    let mainContainer = document.getElementById("table-data");
+    
+    for (let i=0;i<data.length; i++) {
+      // let ind=i+1;
+      // let index=ind.toString();
+      let div = document.createElement("a");
+
+      div.setAttribute('href','blog.html');
+     div.setAttribute("id","refLink")
+     // div.setAttribute('value',index);
+      //div.addEventListener('click','showBlog()');
+      let div2 = document.createElement("div");
+      div2.setAttribute('id','authorName')
+      console.log(data[i].title);
+      div.innerHTML = (i+1)+'. Title: ' + data[i].title+'<br/>'; 
+      mainContainer.appendChild(div);
+      div2.innerHTML= data[i].author+'<br/>';;
+      mainContainer.appendChild(div2);
+      
+    }
+    
+  }
+
+  //=====================================================================
+  
+ // let val=document.getElementById('refLink').value;
+  fetch('http://localhost:2222/blogs/1')
+  .then(response=> {
+    return response.json();
+  })
+  .then(data => {
+    console.log(data);
+    showBlog(data);
+    
+  })
+  .catch(err=> {
+    console.log(err);
+  });
+
+  function showBlog(data){
+
+    let div3= document.getElementById("blogTitle");
+    let div4=document.getElementById("blogBody");
+    let div5=document.getElementById("blogAuthor");
+    console.log(data.title);
+    div3.innerHTML = data.title;
+    div4.innerHTML = data.content;
+    div5.innerHTML = data.author;
+    
+
+  }
